727bf2fe29d74ee998dc2ca368f16935dbeb4685
Artem Uholkov <artem.ugolkov@ukr.net>
2024-10-30 13:32:34 +0200

    added header to all pages

--- js/script.js
+++ js/script.js
@@ -35,3 +35,15 @@ function burgerHandler(e) {
   }
   burger.classList.toggle('active');
 }
+
+var className = 'inverted';
+var scrollTrigger = 60;
+
+window.onscroll = function () {
+  // We add pageYOffset for compatibility with IE.
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName('header')[0].classList.add(className);
+  } else {
+    document.getElementsByTagName('header')[0].classList.remove(className);
+  }
+};
