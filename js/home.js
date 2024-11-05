309ab509fc69842a06bf357601797564c1722e2a
Mari-devop <mari.philon@gmail.com>
2024-11-05 11:22:57 +0200

    full second

--- js/home.js
+++ js/home.js
@@ -10,3 +10,21 @@ document.querySelectorAll('.expand-btn').forEach((btn) => {
         parentContainer.classList.add('expanded');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.slider__menu');
+    const menuOffset = menu.getBoundingClientRect().top + window.scrollY;
+
+    const earlyOffset = 100; 
+
+    window.addEventListener('scroll', function () {
+        const triggerPoint = window.scrollY + window.innerHeight / 2 - earlyOffset;
+
+        if (triggerPoint >= menuOffset) {
+            menu.classList.add('sticky');
+        } else {
+            menu.classList.remove('sticky');
+        }
+    });
+});
